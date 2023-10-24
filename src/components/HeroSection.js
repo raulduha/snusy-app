6a3aa724bba48bb93bef0eef9679d91fa931eb21
raulduha <80332171+raulduha@users.noreply.github.com>
2023-10-23 22:08:23 -0300

    font y mobile view

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,15 +1,27 @@
+import styled, { createGlobalStyle } from 'styled-components';
 import React from 'react';
 import './HeroSection.css';
 
+const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Cinzel:wght@700&family=Bitter&display=swap');
+`;
+
 const HeroSection = () => {
     return (
-        <div className="hero-container">
-            <div className="hero-content">
-                <h1 className="hero-title">SNUSY.cl</h1>
-                <p className="hero-subtitle">Próximamente</p>
+        <>
+            <GlobalStyles />
+            <div className="hero-container">
+                <div className="hero-content">
+                    <div className="hero-card">
+                        <h1 className="hero-title">SNUSY.cl</h1>
+                        <p className="hero-subtitle">Próximamente</p>
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
+
+
 
 export default HeroSection;
