426969c4128545f0bd7853a9f5a89d12fb1a83f5
raulduha <80332171+raulduha@users.noreply.github.com>
2023-10-24 00:41:36 -0300

    logo y nombre de tab y productos disponibles

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,44 +2,42 @@ import React from 'react';
 import './Products.css';
 
 // Importa las imágenes directamente desde la carpeta "assets"
-import productImage1 from '../assets/sn.png';
-import productImage2 from '../assets/sn2.png';
-import productImage3 from '../assets/sn3.png';
+import productImage1 from '../assets/snp1.jpg';
+import productImage2 from '../assets/snp2.jpg';
+import productImage3 from '../assets/snp3.jpg';
+
+const products = [
+    {
+        image: productImage1,
+        title: 'Siberia -80 White Dry    ',
+        description: 'White dry portion with taste of spearmint',
+        price: '8,000 CLP',
+    },
+    {
+        image: productImage2,
+        title: 'VELO Ice cool mint strong slim all white',
+        description: 'Slim all white portion with taste of mint and peppermint',
+        price: '8,000 CLP',
+    },
+    {
+        image: productImage3,
+        title: 'General White Mint Portion',
+        description: 'White portion with a cooling taste of mint',
+        price: '8,000 CLP',
+    },
+];
 
 const Products = () => {
     return (
         <div className="products-container">
-            <div className="product-card">
-                <img src={productImage1} alt="Product 1" loading="lazy"/>
-                <h2>Product 1</h2>
-                <p>Description of Product 1.</p>
-            </div>
-            <div className="product-card">
-                <img src={productImage2} alt="Product 2" loading="lazy"/>
-                <h2>Product 2</h2>
-                <p>Description of Product 2.</p>
-            </div>
-            <div className="product-card">
-                <img src={productImage3} alt="Product 3" loading="lazy"/>
-                <h2>Product 3</h2>
-                <p>Description of Product 3.</p>
-            </div>
-            <div className="product-card">
-                <img src={productImage1} alt="Product 4" loading="lazy"/>
-                <h2>Product 4</h2>
-                <p>Description of Product 4.</p>
-            </div>
-            <div className="product-card">
-                <img src={productImage2} alt="Product 5" loading="lazy"/>
-                <h2>Product 5</h2>
-                <p>Description of Product 5.</p>
-            </div>
-            <div className="product-card">
-                <img src={productImage3} alt="Product 6" loading="lazy"/>
-                <h2>Product 6</h2>
-                <p>Description of Product 6.</p>
-            </div>
-            {/* Agrega más tarjetas de productos según sea necesario */}
+            {products.map((product, index) => (
+                <div className="product-card" key={index}>
+                    <img src={product.image} alt={product.title} loading="lazy" />
+                    <h2>{product.title}</h2>
+                    <p>{product.description}</p>
+                    <p>{product.price}</p>
+                </div>
+            ))}
         </div>
     );
 };
