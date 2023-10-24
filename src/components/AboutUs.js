bf1c53239b1bb0acf8c2d07d612fa54aad73e3e9
raulduha <80332171+raulduha@users.noreply.github.com>
2023-10-23 22:30:44 -0300

    about us y products

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -4,8 +4,14 @@ import './AboutUs.css';
 const AboutUs = () => {
     return (
         <div className="about-us-container">
-            <h2>About Us</h2>
-            <p>Texto descriptivo sobre la empresa...</p>
+            <div className="about-us-card">
+                <h2>About Us</h2>
+                <p>Welcome to Snusy, your online source for the best snus products from around the world. Snus is a smokeless tobacco that you place under your lip and enjoy without any smoke or chemicals. Snus is a safer and healthier alternative to smoking, and it comes in many brands and flavors to suit your taste.
+
+At Snusy, we are passionate about snus and its benefits. We want to share our passion with you and help you find your perfect snus. We have a wide selection of high-quality snus products from reputable manufacturers. We also offer fast and discreet delivery to your door, with no customs issues or extra fees.
+
+We are Snusy, your one-stop shop for all your snus needs. Join us today and discover the world of snus! 🌎</p>
+            </div>
         </div>
     );
 };
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,11 +1,48 @@
 import React from 'react';
 import './Products.css';
 
+// Importa las imágenes directamente desde la carpeta "assets"
+import productImage1 from '../assets/snusy.png';
+import productImage2 from '../assets/snusy2.png';
+import productImage3 from '../assets/snusy3.png';
+import productImage4 from '../assets/snusy4.png';
+import productImage5 from '../assets/snusy5.png';
+import productImage6 from '../assets/circular.png';
+
 const Products = () => {
     return (
         <div className="products-container">
-            <h2>Our Products</h2>
-            {/* Puedes listar tus productos aquí */}
+            <div className="product-card">
+                <img src={productImage1} alt="Product 1" />
+                <h2>Product 1</h2>
+                <p>Description of Product 1.</p>
+            </div>
+            <div className="product-card">
+                <img src={productImage2} alt="Product 2" />
+                <h2>Product 2</h2>
+                <p>Description of Product 2.</p>
+            </div>
+            <div className="product-card">
+                <img src={productImage3} alt="Product 2" />
+                <h2>Product 3</h2>
+                <p>Description of Product 3.</p>
+            </div>
+            <div className="product-card">
+                <img src={productImage1} alt="Product 2" />
+                <h2>Product 4</h2>
+                <p>Description of Product 4.</p>
+            </div>
+            <div className="product-card">
+                <img src={productImage2} alt="Product 2" />
+                <h2>Product 5</h2>
+                <p>Description of Product 5.</p>
+            </div>
+            <div className="product-card">
+                <img src={productImage6} alt="Product 2" />
+                <h2>Product 6</h2>
+                <p>Description of Product 6.</p>
+            </div>
+            {/* Agrega más tarjetas de productos según sea necesario */}
         </div>
     );
 };
