4ca2a014a88a217f7379a0fe9b841974875a6393
raulduha <80332171+raulduha@users.noreply.github.com>
2023-10-29 21:10:59 -0300

    sidebar, sidebar responsive, descripcion

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,28 @@
-// Navbar.js
-
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Navbar.css';
+import Sidebar from './Sidebar'; // Importa el componente Sidebar
 
 const Navbar = () => {
-    const history = useNavigate();
+    const navigate = useNavigate();
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    };
 
     return (
-        <nav>
-            <div className="logo" onClick={() => history('/')}></div>
-            <ul id="navbar">
-                
-            
-                <li><Link to="/products">Productos</Link></li>
-                <li><Link to="/signin">Iniciar sesion</Link></li>
-            </ul>
-        </nav>
+        <>
+            <nav>
+                <div className="sidebar-icon" onClick={toggleSidebar}>&#9776;</div>
+                <div className="logo" onClick={() => navigate('/')}></div>
+                <ul id="navbar">
+                    <li><Link to="/products">Productos</Link></li>
+                    <li><Link to="/signin">Iniciar sesión</Link></li>
+                </ul>
+            </nav>
+            <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+        </>
     );
 };
 
---
+++ src/components/Sidebar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Sidebar.css';
+
+const Sidebar = ({ isOpen, toggleSidebar }) => {
+    return (
+        <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+            <button className="close-btn" onClick={toggleSidebar}>✖</button>
+            <div className="sidebar-content">
+                <h1>Menu</h1>
+                <h2>Nombre</h2>
+                <ul>
+                    <li><Link to="/" onClick={toggleSidebar}>Home</Link></li>
+                    <li><Link to="/products" onClick={toggleSidebar}>Productos</Link></li>
+                    <li><Link to="/signin" onClick={toggleSidebar}>Iniciar sesión</Link></li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
