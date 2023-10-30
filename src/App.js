1e54721e57fce2a490d55340a7f90f9428e86763
raulduha <80332171+raulduha@users.noreply.github.com>
2023-10-30 02:11:41 -0300

    age modal 2

--- src/App.js
+++ src/App.js
@@ -1,35 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
-import AboutUs from './components/AboutUs'; // Asegúrate de tener este componente
-import Products from './components/Products'; // Asegúrate de tener este componente
+import AboutUs from './components/AboutUs'; 
+import Products from './components/Products'; 
 import Footer from './components/Footer';
 import Login from './components/Login';
 import Register from './components/Register';
 import './firebase';
 import { AuthProvider } from './components/AuthProvider';
+import AgeVerificationModal from './components/AgeVerificationModal';
 
 function App() {
+  const [showModal, setShowModal] = useState(true);
+
+  const handleConfirm = () => {
+    setShowModal(false);
+  };
+
+  const handleDeny = () => {
+    alert("Debes ser mayor de 18 años para acceder a este sitio.");
+    setShowModal(false);
+  };
+
   return (
     <AuthProvider>
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/aboutus" element={<AboutUs />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+      <Router>
+        <div className="App">
+          {showModal && <AgeVerificationModal onConfirm={handleConfirm} onDeny={handleDeny} />}
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/aboutus" element={<AboutUs />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
     </AuthProvider>
   );
 }
 
 export default App;
+
 
