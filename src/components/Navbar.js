992a0fe323d1f33fa755bfbea6e1d92fa0e232b8
raulduha <80332171+raulduha@users.noreply.github.com>
2023-10-30 01:18:41 -0300

    usuario navbar, login, register (falta estetica)  y falta popup edad

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Navbar.css';
 import Sidebar from './Sidebar';
-import { useAuth } from './AuthProvider'; // Asegúrate de tener la ruta correcta aquí
+import { useAuth } from './AuthProvider';
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebase';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
     const navigate = useNavigate();
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-    const { user } = useAuth(); // Extrae el usuario del contexto de autenticación
+    const { user } = useAuth();
 
     const toggleSidebar = () => {
         setIsSidebarOpen(!isSidebarOpen);
@@ -27,21 +29,22 @@ const Navbar = () => {
     return (
         <>
             <nav>
-                {/* ... Otros elementos ... */}
+                <div className="sidebar-icon" onClick={toggleSidebar}>&#9776;</div>
+                <div className="logo" onClick={() => navigate('/')}></div>
                 <ul id="navbar">
                     <li><Link to="/products">Productos</Link></li>
                     {user ? (
-                        <>
-                            <li className="user-area" onClick={handleLogout}>
-                                <div className="profile-icon"></div>
-                                {user.displayName}
-                            </li>
-                            <li><Link to="#" onClick={handleLogout}>Cerrar sesión</Link></li>
-                        </>
+                        <li><Link to="#" onClick={handleLogout}>Cerrar sesión</Link></li>
                     ) : (
                         <li><Link to="/login">Iniciar sesión</Link></li>
                     )}
                 </ul>
+                {user && (
+                    <div className="user-area" onClick={handleLogout}>
+                        <FontAwesomeIcon icon={faUser} className="profile-icon" />
+                        {user.displayName}
+                    </div>
+                )}
             </nav>
             <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
         </>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,18 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { signOut } from 'firebase/auth';
+import { auth } from '../firebase';
 import './Sidebar.css';
+import { useAuth } from './AuthProvider'; // Asegúrate de tener la ruta correcta aquí
 
 const Sidebar = ({ isOpen, toggleSidebar }) => {
+    const { user } = useAuth();
+
+    const handleLogout = async () => {
+        try {
+            await signOut(auth);
+            // Aquí, necesitas redirigir al usuario después de cerrar sesión, puedes usar navigate o otro enfoque que tengas en mente.
+        } catch (error) {
+            console.error("Error al cerrar sesión:", error.message);
+        }
+    };
+
     return (
         <div className={`sidebar ${isOpen ? 'open' : ''}`}>
             <button className="close-btn" onClick={toggleSidebar}>✖</button>
             <div className="sidebar-content">
                 <h1>Menu</h1>
-                <h2></h2>
                 <ul>
                     <li><Link to="/" onClick={toggleSidebar}>Home</Link></li>
                     <li><Link to="/products" onClick={toggleSidebar}>Productos</Link></li>
-                    <li><Link to="/login" onClick={toggleSidebar}>Iniciar sesión</Link></li>
+                    {user ? (
+                        <li><Link to="#" onClick={() => { toggleSidebar(); handleLogout(); }}>Cerrar sesión</Link></li>
+                    ) : (
+                        <li><Link to="/login" onClick={toggleSidebar}>Iniciar sesión</Link></li>
+                    )}
                 </ul>
             </div>
         </div>
